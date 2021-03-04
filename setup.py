47403a27431f85d99f672473168e7aa8904a292d
Sidney Radcliffe <sidneyradcliffe@sky.com>
2021-03-04 11:34:09 +0000

    moved version to __init__.py. simplified setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 import pathlib
 import setuptools
-
-long_description = pathlib.Path("README.md").read_text()
-version = pathlib.Path("VERSION").read_text()
+import nb_as_module
 
 setuptools.setup(
     name="nb_as_module",
-    version=version,
+    version=nb_as_module.__version__,
     author="Sidney Radcliffe",
     author_email="sidneyradcliffe@gmail.com",
     description="Load a Jupyter Notebook as a module object.",
-    long_description=long_description,
+    long_description=pathlib.Path("README.md").read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/sradc/nb_as_module",
     license="BSD",
     packages=setuptools.find_packages(),
-    install_requires=["IPython","nbformat"],
+    install_requires=["IPython", "nbformat"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    include_package_data=True,
 )
